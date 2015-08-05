9e05c0a2c705cfb3f86ad04d902e629ef16b3510
François <briatte@gmail.com>
2015-08-05 20:28:13 +0200

    bugfix

--- build.r
+++ build.r
@@ -83,8 +83,8 @@ for (ii in a$legislature %>% unique %>% sort) {
 
   n %n% "country" = meta[ "cty" ] %>% as.character
   n %n% "lang" = meta[ "lang" ] %>% as.character
-  n %n% "years" = legislatures[ as.character(ii) ]
-  n %n% "legislature" = ii
+  n %n% "years" = legislatures[ as.character(ii) ] %>% as.character
+  n %n% "legislature" = ii %>% as.character
   n %n% "chamber" = meta[ "ch" ] %>% as.character
   n %n% "type" = meta[ "type" ] %>% as.character
   n %n% "ipu" = meta[ "ipu" ] %>% as.integer
